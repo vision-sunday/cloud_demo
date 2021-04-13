cdbe72b616c900c550d1b2acee54314fc0dde623
cloud_demo <879042343@qq.com>
2021-04-13 23:10:52 +0800

    加了点注释

--- src/main/java/com/example/demo/SpringclouddemoApplication.java
+++ src/main/java/com/example/demo/SpringclouddemoApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringclouddemoApplication {
 
     public static void main(String[] args) {
-
+        //加了注释
         SpringApplication.run(SpringclouddemoApplication.class, args);
     }
 
