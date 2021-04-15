c53df64c4de1c5f1f58a2962bfe53d3b92cc0bbe
zhangyong <879042343@qq.com>
2021-04-15 10:46:14 +0800

    是在没办反，暂时这样写

--- springcloud-eureka-7001/src/main/java/com/example/demo/SpringcloudEureka7001Application.java
+++ springcloud-eureka-7001/src/main/java/com/example/demo/SpringcloudEureka7001Application.java
@@ -1,13 +1,15 @@
-package com;
+package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-@EnableEurekaServer //服务启动类
-public class EurekaApplication {
+@EnableEurekaServer
+public class SpringcloudEureka7001Application {
+
     public static void main(String[] args) {
-        SpringApplication.run(EurekaApplication.class, args);
+        SpringApplication.run(SpringcloudEureka7001Application.class, args);
     }
+
 }
